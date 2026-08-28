e8635f57f8cf2cf86261d4c89898d38880a7637b
Benex872 <bleonhasani84@gmail.com>
2026-08-28 14:42:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Wie viele Punkte hast du in deiner Prüfung erreicht?");
+
+int punkte = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Wie viel ist die Maxunktzahl?");
+
+int maxpunkte = Convert.ToInt32(Console.ReadLine());
+
+double Note = ((double)punkte / maxpunkte * 5 + 1);
+
+if (punkte > maxpunkte)
+
+{
+
+    Console.WriteLine("WOWWW SIE HABEN DIE MAXPINKTZAHL ÜBERTROFFEN IHRE NOTE IST EINE 6+");
+
+}
+
+else
+
+{
+
+    Console.Write("Ihre Note ist " + Note);
+
+}
+
+
