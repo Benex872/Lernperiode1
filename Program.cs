c11a518b1f72aa42f60d3598a83cdc014f993bc9
Benex872 <bleonhasani84@gmail.com>
2026-08-28 14:35:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("===== True or False Quiz=====");
+int punkte = 0;
+
+Console.WriteLine("Sind sie ein Mensch true/false? ");
+bool antwort1= Convert.ToBoolean(Console.ReadLine());
+if (antwort1 == true)
+{
+    Console.WriteLine("Das ist Richtig!!");
+    punkte++;
+}
+else
+{
+    Console.WriteLine("Das ist Falsch!!");
+}
+
+Console.WriteLine("Sind sie ein Tier true/false? ");
+bool antwort2= Convert.ToBoolean(Console.ReadLine());
+if (antwort2 == true)
+{
+    Console.WriteLine("Das ist Richtig!!");
+    punkte++;
+}
+else
+{
+    Console.WriteLine("Das ist Falsch!!");
+}
+Console.WriteLine("Sind sie ein Roboter true/false? ");
+bool antwort3= Convert.ToBoolean(Console.ReadLine());
+if (antwort3 == true)
+{
+    Console.WriteLine("Das ist Richtig!!");
+    punkte++;
+}
+else
+{
+    Console.WriteLine("Das ist Falsch!!");
+
+}
+Console.WriteLine("Sie haben " + punkte + "  ereicht!! ");
