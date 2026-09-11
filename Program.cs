9a0a9044b2f5b32e8c821bd0f363e8b4d7936459
Benex872 <bleonhasani84@gmail.com>
2026-09-11 16:16:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[] fragen = new string[10];
+
+fragen[0] = "Was ist 2 + 2?";
+fragen[1] = "Wie viele Tage hat eine Woche?";
+fragen[2] = "Was ist die Hauptstadt der Schweiz?";
+fragen[3] = "Wie viel ist 5 * 5?";
+fragen[4] = "Welche Farbe entsteht aus Blau und Gelb?";
+fragen[5] = "Wie viele Monate hat ein Jahr?";
+fragen[6] = "Was ist 10 - 3?";
+fragen[7] = "Wie viele Stunden hat ein Tag?";
+fragen[8] = "Was ist 3 * 3?";
+fragen[9] = "Wie viele Minuten hat eine Stunde?";
+
+Console.WriteLine("Wie viele Fragen möchtest du?");
+int anzahl = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < anzahl; i++)
+{
+    Console.WriteLine(fragen[i]);
+}
