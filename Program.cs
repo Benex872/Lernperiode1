bca0a23b475367e8935cdb1d506fa8689233d6b4
Benex872 <bleonhasani84@gmail.com>
2026-09-18 15:52:16 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=====Altersrechner=====");
+Console.WriteLine("In Welchem Jahr wurden Sie geboren");
+double year = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("In welchem Jahr befinden wir uns gerade?");
+double currentYear = Convert.ToDouble(Console.ReadLine());
+
+ double age = currentYear - year;
+Console.WriteLine("Sie sind " + age + " Jahre alt");
+Console.ReadLine();
